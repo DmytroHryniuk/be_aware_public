726184cfe208f099326b03423dd061ce588913b6
hryniuk.mitya@gmail.com <2q5A9YASGHSjgQ7>
2020-03-29 00:33:11 +0200

    Added new icon, and chart start

--- app/src/main/java/com/hryniuk/beaware/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/hryniuk/beaware/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,14 @@
 package com.hryniuk.beaware.ui.notifications;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,7 +16,10 @@ import android.support.v4.app.Fragment;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 
+import com.hryniuk.beaware.MainActivity;
 import com.hryniuk.beaware.R;
+
+import java.util.Objects;
 
 public class NotificationsFragment extends Fragment {
 
@@ -19,6 +27,21 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        TextView tv = new TextView(getActivity());
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+
+        tv.setLayoutParams(lp);
+        tv.setText("Chart");
+        tv.setTextSize(20);
+        tv.setTextColor(Color.parseColor("#FFFFFF"));
+        Typeface tf = ResourcesCompat.getFont(Objects.requireNonNull(getActivity()), R.font.comfortaa_bold);
+        tv.setTypeface(tf);
+        Objects.requireNonNull(((MainActivity) getActivity()).getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        Objects.requireNonNull(((MainActivity) getActivity()).getSupportActionBar()).setCustomView(tv);
+
+
+
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
